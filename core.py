d0e54651a34b0cc217fc65d8c84c457927a1fc8c
H1d3l <hidels.rios@gmail.com>
2020-01-24 11:47:39 -0300

    inicio

--- core.py
+++ core.py
@@ -4,8 +4,22 @@ import json
 try:
     file_json = open('source_file_2.json','r')
     data_json = json.load(file_json)
-    print(data_json)
 
+    dict_managers = {}
+    list_managers = []
+    for value in data_json:
+        for i in value['managers']:
+            #if(i not in dict_managers):
+            #if(i in value['managers'] and i not in dict_managers):
+            dict_managers.update({'managers':i,'name':list_managers.append(value['name'])})  
+            
+                #dict_managers.update({'managers':i,'name':list_managers.append(value['name'])   })
+                #print(i,value['managers'])
+            #print(list_managers)
+            #print(True,i,value['managers'],value['name'],dict_managers)
+        print(dict_managers)        
+        
+        
 except Exception as erro:
     print("ocorreu um erro ao carregar o arquivo")
     print("o erro é:{}".format(erro))
