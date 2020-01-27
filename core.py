a7c519e61093cd659d59cff65571a181150356fe
H1d3l <hidels.rios@gmail.com>
2020-01-27 10:26:34 -0300

    teste finalizado

--- core.py
+++ core.py
@@ -2,32 +2,49 @@ import json
 from operator import itemgetter
 
 
-#try:
-file_json = open('source_file_2.json','r')
-data_json = json.load(file_json)
-data_json_sorted = sorted(data_json,key=itemgetter('priority'))
+try:
+    file_json = open('source_file_2.json','r')
+    data_json = json.load(file_json)
+    data_json_sorted = sorted(data_json,key=itemgetter('priority'))
 
-list_managers = []
-list_projects = []
-dict_managers = {}
 
-for values in data_json_sorted:
-    for i in values:
-        for k in values['managers']:
-            if(k not in dict_managers):
-                dict_managers.update({k:[values[ 'name']]})
-            else:
-                dict_managers[k].append(values['name'])
-        '''
-        if(value['managers'] in dict_managers):
-            dict_managers.update({value['managers']:value['name']})
+    dict_managers = {}
+    dict_watchers = {}
 
-        else:
-            dict_managers['name'].append(value['name'])
-'''
-            
+    for values in data_json_sorted:
+        for i in values:
+            for k in values['managers']:
+                if(k not in dict_managers):
+                    dict_managers.update({k:[values[ 'name']]})
+                else:
+                    if(values['name'] in dict_managers[k]):
+                        pass
+                    else:
+                        dict_managers[k].append(values['name'])
 
-print(json.dumps(dict_managers, sort_keys=True, indent=4))
-#except Exception as erro:
- #   print("ocorreu um erro ao carregar o arquivo")
-  #  print("o erro é:{}".format(erro))
+    for values in data_json_sorted:
+        for i in values:
+            for k in values['watchers']:
+                if(k not in dict_watchers):
+                    dict_watchers.update({k:[values[ 'name']]})
+                else:
+                    if(values['name'] in dict_watchers[k]):
+                        pass
+                    else:
+                        dict_watchers[k].append(values['name'])
+
+    dict_managers_json = json.dumps(dict_managers, sort_keys=True, indent=4)
+    dict_watchers_json = json.dumps(dict_watchers, sort_keys=True, indent=4)
+
+
+    save_dict_managers = open('managers.json','w')
+    save_dict_watchers = open('watchers.json','w')
+    save_dict_managers.write(dict_managers_json)
+    save_dict_watchers.write(dict_watchers_json)
+
+    save_dict_managers.close()
+    save_dict_watchers.close()
+
+except Exception as erro:
+    print("ocorreu um erro ao carregar o arquivo")
+    print("o erro é:{}".format(erro))
