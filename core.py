4789cb2a2407a6120e82596f101f0e8226a6d5f6
H1d3l <hidels.rios@gmail.com>
2020-01-27 09:57:14 -0300

    inicio

--- core.py
+++ core.py
@@ -2,21 +2,32 @@ import json
 from operator import itemgetter
 
 
-try:
-    file_json = open('source_file_2.json','r')
-    data_json = json.load(file_json)
-    data_json_sorted = sorted(data_json,key=itemgetter('priority'))
+#try:
+file_json = open('source_file_2.json','r')
+data_json = json.load(file_json)
+data_json_sorted = sorted(data_json,key=itemgetter('priority'))
 
-    dict_managers = {}
+list_managers = []
+list_projects = []
+dict_managers = {}
 
-    for value in data_json_sorted:
-        for i in value['managers']:
-            if(i in value['managers']):
-                dict_managers.update({'managers':i,'name':value['name']})
-                print(dict_managers)
+for values in data_json_sorted:
+    for i in values:
+        for k in values['managers']:
+            if(k not in dict_managers):
+                dict_managers.update({k:[values[ 'name']]})
+            else:
+                dict_managers[k].append(values['name'])
+        '''
+        if(value['managers'] in dict_managers):
+            dict_managers.update({value['managers']:value['name']})
 
+        else:
+            dict_managers['name'].append(value['name'])
+'''
+            
 
-    #print(json.dumps(data_json_sorted, sort_keys=True, indent=4))
-except Exception as erro:
-    print("ocorreu um erro ao carregar o arquivo")
-    print("o erro é:{}".format(erro))
+print(json.dumps(dict_managers, sort_keys=True, indent=4))
+#except Exception as erro:
+ #   print("ocorreu um erro ao carregar o arquivo")
+  #  print("o erro é:{}".format(erro))
